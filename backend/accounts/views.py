69770ab38676367f926a092c271edcd3b30d5491
Shivam Gupta <iamshivam0512005@gmail.com>
2026-02-16 23:30:00 +0530

    create user file

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -8,6 +8,8 @@ from .serializers import LoginSerializer,CreateUserSerializer
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+
+#REGISTRATION view
 class RegistrationView(APIView):
 
     def post(self, request):
@@ -40,40 +42,14 @@ class LoginView(APIView):
         try:
             serializer.is_valid(raise_exception=True)
         except ValidationError as e:
-            # directly dict return karo jo serializer me defined hai
+           
             return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
-
-# class CreateUserView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def post(self, request):
-#         serializer = CreateUserSerializer(
-#             data=request.data,
-#             context={"request": request}
-#         )
-
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             return Response(
-#                 {
-#                     "success": True,
-#                     "message": "User created successfully",
-#                     "data": {
-#                         "id": user.id,
-#                         "name": user.first_name,
-#                         "email": user.email,
-#                         "role": user.role
-#                     }
-#                 },
-#                 status=status.HTTP_201_CREATED
-#             )
-
-#         return Response(serializer.errors, status=400)        
-
+      
         
+#create user view
 
 class CreateUserView(APIView):
     permission_classes = [IsAuthenticated]
