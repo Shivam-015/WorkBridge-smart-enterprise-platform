bdfb18543efbe46232238e1cdd22c5849e727280
Shivam Gupta <iamshivam0512005@gmail.com>
2026-03-11 18:00:36 +0530

    changes in dashboard and user details page

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,7 +152,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
-FRONTEND_BASE_URL = os.getenv("FRONTEND_BASE_URL", "http://localhost:5173")
+FRONTEND_BASE_URL = os.getenv("FRONTEND_BASE_URL", default="")
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
