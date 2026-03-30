66a5781e35b1876d280ef280663f9c20a5ab424c
Shivam Gupta <iamshivam0512005@gmail.com>
2026-03-30 15:30:45 +0530

    fix media url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,18 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/', include('dashboard.urls')),
-    path('api/', include('tasks.urls')) , 
+    path('api/', include('tasks.urls')),
     path('api/', include('companies.urls')),
     path('api/', include('projects.urls')),
     path('api/', include('hr.urls')),
-
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
